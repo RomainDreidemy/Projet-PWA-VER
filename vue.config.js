73af57573aa1ef996f12e807ca63b4c3d9200216
kryweb <dreidemyromain@gmail.com>
2020-05-18 14:56:44 +0200

    Manifest

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	pwa: {
+		name: 'Notre super blog',
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+		manifestOptions: {
+			display: 'fullscreen'
+		}
+	}
+}
